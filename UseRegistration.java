3bf76cc28e79f1073631e9c90510777b20b65e2d
jiendra <jitendra@banshkar.com>
2022-09-15 23:12:08 +0530

    UC_3

--- UseRegistration.java
+++ UseRegistration.java
@@ -2,21 +2,21 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class UseRegistration {
-    public  void  valid(String lastName){
-        Pattern pattern = Pattern.compile("^[A-Z]{3}");
-        Matcher check = pattern.matcher(lastName);
+    public  void  validEmail(String email){
+        Pattern pattern = Pattern.compile("^[ a-zA-Z]+.+[x-z]?(@bi.co.in)+$");
+        Matcher check = pattern.matcher( email);
         if(check.matches()){
-            System.out.println("last Name  is Valid");
+            System.out.println(" email is Valid");
         }
         else {
-            System.out.println(" last Name not Valid ! please Enter valid Name");
+            System.out.println(" email  not Valid ! please Enter valid Name");
         }
     }
     public static void main(String[] args) {
         UseRegistration user =new UseRegistration();
         Scanner sc =new Scanner(System.in);
-        System.out.println("Enter last Name :");
-        String lastName =sc.nextLine();
-        user.valid(lastName);
+        System.out.println("Enter  email :");
+        String  email =sc.nextLine();
+        user.validEmail(email);
     }
 }
